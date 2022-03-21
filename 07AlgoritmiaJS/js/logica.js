7ca7f60c14d55cdbdb42f8d44ed6d8c418b78817
3mm4NUE1 <ehernandezm2007@alumno.ipn.mx>
2022-03-21 01:52:50 -0600

    Algoritmia

--- 07AlgoritmiaJS/js/logica.js
+++ 07AlgoritmiaJS/js/logica.js
@@ -1,4 +1,58 @@
+//problema 1
 
+function problema1(){
+    var validar = /^[0-9]+$/;
+    var oracion = document.getElementById("p1-input").value;
+
+    if(oracion.match(validar)){
+        alert("Solo ingresa palabras");
+    }
+    else if(oracion == ""){
+        document.getElementById("p1-output").textContent = "No hay palabras"
+    }
+    else{
+        var p1_palabras = oracion.split(" ")        
+        document.getElementById("p1-output").textContent = p1_palabras.reverse()
+    }
+}
+
+//problema 2
+
+function problema2(){
+    var cx1 = document.getElementById("problema2-x1").value;
+    var cx2 = document.getElementById("problema2-x2").value;
+    var cx3 = document.getElementById("problema2-x3").value;
+    var cx4 = document.getElementById("problema2-x4").value;
+    var cx5 = document.getElementById("problema2-x5").value;
+
+    var cy1 = document.getElementById("problema2-y1").value;
+    var cy2 = document.getElementById("problema2-y2").value;
+    var cy3 = document.getElementById("problema2-y3").value;
+    var cy4 = document.getElementById("problema2-y4").value;
+    var cy5 = document.getElementById("problema2-y5").value;
+
+    var x1 = parseFloat(cx1)
+    var x2 = parseFloat(cx2)
+    var x3 = parseFloat(cx3)
+    var x4 = parseFloat(cx4)
+    var x5 = parseFloat(cx5)
+
+    var y1 = parseFloat(cy1)
+    var y2 = parseFloat(cy2)
+    var y3 = parseFloat(cy3)
+    var y4 = parseFloat(cy4)
+    var y5 = parseFloat(cy5)
+
+    if(cx1==""||cx2==""||cx3==""||cx4==""||cx5==""||cy1==""||cy2==""||cy3==""||cy4==""||cy5==""){
+        document.getElementById("p2-output").textContent = "No hay nada para sumar"
+    }
+    else{
+        var resultado = (x1*y1)+(x2*y2)+(x3*y3)+(x4*y4)+(x5*y5)
+
+        document.getElementById("p2-output").textContent = "El reslutado es: " + resultado
+    }
+
+}
 
 //problema 3
 
